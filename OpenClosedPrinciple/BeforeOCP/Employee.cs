7280479da5867b2a3422792b152cdaf10c7bdde4
Nimap <Nimap@DESKTOP-22H8R70>
2022-11-26 12:14:29 +0530

    return method change

--- OpenClosedPrinciple/BeforeOCP/Employee.cs
+++ OpenClosedPrinciple/BeforeOCP/Employee.cs
@@ -10,18 +10,18 @@ namespace OpenClosedPrinciple.BeforeOCP
     {
         public double CalculateBonus(string emptype, double salary)
         {
-            double bonus = 0;
+            return 0;
             if (emptype == "CompanyEmployee")
             {
-                bonus = ((salary * 10) / 100);
+                return ((salary * 10) / 100);
             }
             else if (emptype == "ContractEmployee")
             {
-                bonus = 0;
+                return 0;
             }
             else if (emptype == "ThirdParty")
             {
-                bonus = 0;
+                return 0;
             }
         }
     }
