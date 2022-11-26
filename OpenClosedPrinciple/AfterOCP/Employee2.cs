4636ae1a70e18e9012dc2f3367d10beefd987a27
Nimap <Nimap@DESKTOP-22H8R70>
2022-11-26 12:10:14 +0530

    added the OPEN CLOSED PRINCIPLE

---
+++ OpenClosedPrinciple/AfterOCP/Employee2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosedPrinciple.AfterOCP
+{
+    class Employee1
+    {
+        abstract class Bonus
+        {
+            public abstract double CalculateBonus(double salary);
+
+        }
+        class CompanyEmployee:Bonus
+        {
+            public override double CalculateBonus(double salary)
+            {
+                return ((salary * 0.10) / 100);
+            }
+        }
+        class ContractEmployee:Bonus
+        {
+            public override double CalculateBonus(double salary)
+            {
+                return 0;
+            }
+        }
+        class ThirdParty : Bonus
+        {
+            public override double CalculateBonus(double salary)
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ OpenClosedPrinciple/BeforeOCP/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenClosedPrinciple.BeforeOCP
+{
+    public class Employee
+    {
+        public double CalculateBonus(string emptype, double salary)
+        {
+            double bonus = 0;
+            if (emptype == "CompanyEmployee")
+            {
+                bonus = ((salary * 10) / 100);
+            }
+            else if (emptype == "ContractEmployee")
+            {
+                bonus = 0;
+            }
+            else if (emptype == "ThirdParty")
+            {
+                bonus = 0;
+            }
+        }
+    }
+
+}
